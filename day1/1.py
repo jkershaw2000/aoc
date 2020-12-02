b612931fb9f8a5903b8f18d73aa923720ffe1ecf
jkershaw2000 <jackkersh2000@gmail.com>
2020-12-02 10:32:54 +0000

    Cleanup

--- day1/1.py
+++ day1/1.py
@@ -6,22 +6,22 @@ def get_input():
     with open("./day1/1.in","r") as f:
         return [int(line.strip()) for line in f.readlines()]
     
-def part1():
+def p1():
     data = get_input()
     pairs = combinations(data,2)
     return [pair[1] * pair[0] for pair in pairs if pair[1] + pair[0] == 2020]
 
-def part2():
+def p2():
     data = get_input()
     triplets = combinations(data, 3)
     return [triplet[2] * triplet[1] * triplet[0] for triplet in triplets if triplet[2] + triplet[1] + triplet[0] == 2020]
 
 p1start = timer()
-p1 = part1()
+p1 = p1()
 p1end = timer()
 print(f"Part 1: {p1} in {p1end-p1start}s.")
 
 p2start = timer()
-p2 = part2()
+p2 = p2()
 p2end = timer()
 print(f"Part 2: {p2} in {p2end-p2start}s.")
