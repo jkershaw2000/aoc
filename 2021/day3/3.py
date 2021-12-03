28ad611ba9c98f1b009bc229f0deef76ea838a46
Jack Kershaw <51750850+jkershaw2000@users.noreply.github.com>
2021-12-03 11:19:50 +0000

    Refactored

--- 2021/day3/3.py
+++ 2021/day3/3.py
@@ -1,75 +1,48 @@
-import copy
 with open("2021/day3/3.in", "r") as f:
     data = [num.strip() for num in f.readlines()]
 
-gamma = ""
-epsilon = ""
-
-for i in range(len(data[0])):
-    ones = 0
-    zeros = 0
+def count_bits(data, i):
+    ones, zeros = 0, 0
     for val in data:
         if val[i] == "0":
             zeros += 1
         else:
-            ones += 1
+            ones += 1  
+    return zeros, ones
 
-    if zeros > ones:
-        gamma += "1"
-        epsilon += "0"
-    else:
-        gamma += "0"
-        epsilon += "1"
+def get_new_search_space(search_space, i, check):
+    new_space = []
+    for v in search_space:
+        if v[i] == check:
+            new_space.append(v)
+    return new_space
 
-search_space = copy.deepcopy(data)
-for i in range(len(data[0])):
-    ones = 0
-    zeros = 0
-    for val in search_space:
-        if val[i] == "0":
-            zeros += 1
+def part1():
+    gamma = ""
+    epsilon = ""
+    for i in range(len(data[0])):
+        zeros, ones = count_bits(data, i)
+        if zeros > ones:
+            gamma += "1"
+            epsilon += "0"
         else:
-            ones += 1
-    temp = []
-    if zeros > ones:
-        for v in search_space:
-            if v[i] == "0":
-                temp.append(v)
-    else:
-        for v in search_space:
-            if v[i] == "1":
-                temp.append(v)
-    search_space = temp
-ox = search_space[0]
+            gamma += "0"
+            epsilon += "1"
+    return int(gamma, base=2) * int(epsilon, base=2)
 
-search_space = copy.deepcopy(data)
-for i in range(len(data[0])):
-    ones = 0
-    zeros = 0
-    if len(search_space) == 1:
-        break
-    for val in search_space:
-        if val[i] == "0":
-            zeros += 1
+def part2(data, bit):
+    search_space = data
+    for i in range(len(data[0])):
+        zeros, ones = count_bits(search_space, i)
+        if zeros > ones:
+            temp = get_new_search_space(search_space, i, str(int(not bit)))
         else:
-            ones += 1
-    temp = []
-    if ones > zeros or ones == zeros:
-        for v in search_space:
-            if v[i] == "0":
-                temp.append(v)
-    else:
-        for v in search_space:
-            if v[i] == "1":
-                temp.append(v)
-    search_space = temp
-    print(search_space)       
+            temp = get_new_search_space(search_space, i, str(int(bit)))
+        search_space = temp
+        if len(search_space) == 1:
+            return search_space[0]
 
-car = search_space[0]
-print(int(ox, base=2), int(car, base=2))
-
-part1 = int(gamma, base=2) * int(epsilon, base=2)
-part2 = int(ox, base=2) * int(car, base=2)
-
+part1 = part1()
+part2 = int(part2(data, True), base=2)*int(part2(data, False), base=2)
 print(f"Part1: {part1}")
 print(f"Part2: {part2}")
