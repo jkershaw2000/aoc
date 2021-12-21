5e1232b5bf27aa21292d27dba4263e3189caaff9
Jack Kershaw <51750850+jkershaw2000@users.noreply.github.com>
2021-12-21 15:41:33 +0000

    Minor optimisation for padding

--- 2021/day20/20.py
+++ 2021/day20/20.py
@@ -6,9 +6,6 @@ with open("2021/day20/20.in", "r") as f:
     f.readline()  # Skip over line
     image = [[c for c in line.strip()] for line in f.readlines()]
 
-# Pad image so can apply to an "infinite" area
-infinite_image = np.pad(image, (5, 5), mode="constant", constant_values=".").tolist()
-
 
 def get_neighbours(row, col):
     for i in row - 1, row, row + 1:
@@ -16,11 +13,14 @@ def get_neighbours(row, col):
             yield i, j
 
 
-padding = (".", "#")
-
-
-def solve(infinite_image, runs=2):
+def solve(image, runs=2):
+    PADDING = (".", "#")
     for run in range(runs):
+        # Pad image so can apply to an "infinite" area
+        infinite_image = np.pad(
+            image, (2, 2), mode="constant", constant_values=PADDING[run % 2]
+        ).tolist()
+
         new_image = [
             ["." for _ in range(len(infinite_image[0]))]
             for _ in range(len(infinite_image))
@@ -33,25 +33,22 @@ def solve(infinite_image, runs=2):
                     infinite_image[i][j]
                     if (i >= 0 and i < len(infinite_image))
                     and (j >= 0 and j < len(infinite_image[0]))
-                    else padding[run % 2]
+                    else PADDING[run % 2]
                     for i, j in neighbours
                 ]
 
                 lookup = "".join("0" if pixel == "." else "1" for pixel in sub_image)
                 new_image[row][col] = image_enchancement[int(lookup, 2)]
 
-        infinite_image = new_image
+        image = new_image
 
     count = Counter()
-    for row in infinite_image:
+    for row in image:
         count += Counter(row)
     return count["#"]
 
 
-part1 = solve(infinite_image, runs=2)
-infinite_image = np.pad(
-    image, (110, 110), mode="constant", constant_values="."
-).tolist()
-part2 = solve(infinite_image, runs=50)
+part1 = solve(image, runs=2)
+part2 = solve(image, runs=50)
 print(f"Part 1: {part1}")
 print(f"part 2: {part2}")
